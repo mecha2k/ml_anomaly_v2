025630f2a4ab408f69cd40d7594b2019ea1d28d4
mecha2k <mecha2k@naver.com>
2024-09-03 10:43:28 +0900

    init

--- data_analysis.py
+++ data_analysis.py
@@ -5,10 +5,6 @@ import matplotlib.pyplot as plt
 import pickle
 from pathlib import Path
 from datetime import datetime
-
-from matplotlib.lines import lineStyles
-from sympy.benchmarks.bench_meijerint import alpha
-
 from utils import check_graphs_v3
 
 
@@ -89,19 +85,24 @@ def drop_columns(df):
     return df.drop(columns=drop_columns)
 
 
-def plot_train_test_dist(train_df, pieces=10, img_path=None, mode="train"):
-    ncols = len(train_df.columns.values)
+def plot_train_test_dist(df, pieces=10, img_path=None, mode="train"):
+    if mode == "preds":
+        anomalies = df["anomaly"].values
+        df = df.drop(columns=["anomaly"])
+    ncols = len(df.columns.values)
     num_plots = np.round(ncols / pieces, 0).astype(int)
-    xticks = np.arange(0, len(train_df), 50000)
+    xticks = np.arange(0, len(df), 50000)
     fig, ax = plt.subplots(pieces, 1, figsize=(12, 3 * pieces))
     for i in range(pieces):
         start = i * num_plots
         end = min(start + num_plots, ncols)
-        ax[i].plot(train_df.iloc[:, start:end])
+        ax[i].plot(df.iloc[:, start:end])
+        if mode == "preds":
+            ax[i].plot(anomalies, color="cyan", alpha=0.3, linewidth=5)
         ax[i].set_xticks(xticks)
         ax[i].set_title(f"Columns {start} to {end}")
         ax[i].ticklabel_format(style="scientific", axis="x", scilimits=(0, 0))
-        ax[i].legend(train_df.columns[start:end], loc="upper right")
+        ax[i].legend(df.columns[start:end], loc="upper right")
         ax[i].grid()
         ax[i].set_ylim(-0.5, 1.5)
         ax[i].axhline(0, color="r", alpha=0.5)
@@ -116,10 +117,34 @@ if __name__ == "__main__":
 
     train_df = pd.read_pickle(data_path / "train.pkl")
     test_df = pd.read_pickle(data_path / "test.pkl")
-    test_timestamps = pd.read_pickle(data_path / "test_timestamps.pkl")
 
-    plot_train_test_dist(train_df, pieces=17, img_path=image_path, mode="train")
-    plot_train_test_dist(test_df, pieces=17, img_path=image_path, mode="test")
+    # plot_train_test_dist(train_df, pieces=17, img_path=image_path, mode="train")
+    # plot_train_test_dist(test_df, pieces=17, img_path=image_path, mode="test")
+
+    margins = 0.2
+    anomaly_cols = []
+    predictions = np.zeros_like(test_df.values).transpose()
+    for i, col in enumerate(test_df.columns):
+        data = test_df[col].values
+        loc_max = np.where(data > 1 + margins)[0]
+        loc_min = np.where(data < 0 - margins)[0]
+        if len(loc_max) or len(loc_min) > 0:
+            anomaly_cols.append(col)
+            predictions[i, loc_max] = 1
+            predictions[i, loc_min] = 1
+            loc = np.where(predictions[i] == 1)[0]
+            anomay_ratio = len(loc) / len(data) * 100
+            print(f"Column {col} has {len(loc)} anomalies and {anomay_ratio:.2f}%")
+    print(f"Total {len(anomaly_cols)} columns have anomalies")
+    print(f"Total {predictions.sum()} anomalies detected")
+    print(f"Colums with anomalies: {anomaly_cols}")
+
+    preds_df = pd.DataFrame(predictions.transpose(), columns=test_df.columns)
+    preds_df["anomaly"] = preds_df.max(axis=1).astype(int)
+    print(preds_df.sum(axis=0))
+
+    test_df["anomaly"] = preds_df["anomaly"]
+    plot_train_test_dist(test_df, pieces=17, img_path=image_path, mode="preds")
 
     # with open(data_path / "test_anomaly.pkl", "wb") as f:
     #     data_dict = {
@@ -148,12 +173,12 @@ if __name__ == "__main__":
     #     anomalies,
     #     threshold=threshold["recon"],
     #     img_path=img_path,
-    #     mode="test",
+    #     mode="test"
     # )
-    #
-    # sample_submission = pd.read_csv(data_path / "sample_submission.csv")
-    # sample_submission["anomaly"] = anomalies
-    # sample_submission.to_csv(
-    #     data_path / "final_submission.csv", encoding="UTF-8-sig", index=False
-    # )
-    # print(sample_submission["anomaly"].value_counts())
+
+    sample_submission = pd.read_csv(data_path / "sample_submission.csv")
+    sample_submission["anomaly"] = preds_df["anomaly"]
+    sample_submission.to_csv(
+        data_path / "final_submission.csv", encoding="UTF-8-sig", index=False
+    )
+    print(sample_submission["anomaly"].value_counts())
